c730ccd290a954f67d93357a79a477a2bdc26631
Tony-Makdissy <tony.makdissy.sy@gmail.com>
2023-08-03 12:52:42 +0200

    turned shape_fill into class, have some TODOs lift!

--- shape_fill.py
+++ shape_fill.py
@@ -2,59 +2,111 @@ import svgpathtools as svg
 import numpy as np
 import time
 
+
 # TODO: add a foolproof way to check if the path is closed
-# TODO: test some more shapes, espicially when you have a vertex intersecting with a line
+# TODO: test some more shapes, especially when you have a vertex intersecting with a line
 # TODO: decide what to do if the file contains multiple paths, probably for-loop over them
+# TODO: color code the lines according to the direction of the path
+# TODO: add safety distance to the lines, check if the lines are inside the shape with a distance of 0.5mm for example
 
-circle_path = svg.Path(svg.Arc(start=45, radius=45+45j, rotation=0, large_arc=1, sweep=1, end=135),
-        svg.Arc(start=135, radius=45+45j, rotation=0, large_arc=1, sweep=1, end=45))
-# svg.disvg(circle_path)
-# time.sleep(1)
+class ShapeFillLine:
+    def __init__(self, path, distance=2, rotation=0):
+        self.cnt = 0
+        self.path = path
+        self.distance = distance
+        self.rotation = rotation
+        # self.check_for_errors() # TODO: check if I really need this
+        self.pattern = self.get_pattern_lines()
+        self.intersections_points = self.get_intersections_points()
+        self.filling_lines = self.get_filling_lines()
 
-paths, attributes, svg_attributes = svg.svg2paths2('bitmap_Layer 1_1.svg')
-#
-# print(paths[3])
-# svg.disvg(paths[3])
-circle_path = paths[3] # TODO: fix this crab, it's not a circle!
+    def check_for_errors(self):
+        # check if you have a path
+        if type(self.path) != svg.Path:
+            raise ValueError("The path should be of type svgpathtools.path.Path")
+        # check if the path is closed
+        if not self.path.isclosed():
+            raise ValueError("The path is not closed")
+
+    def get_pattern_lines(self, center=90 + 90j, side=360):
+        x_lower_limit = center.real - side / 2
+        # x_upper_limit: it should be (center.real + side/2)
+        # but I need to deal with floating point errors in number_of_steps
+        y_lower_limit = center.imag - side / 2
+        y_upper_limit = center.imag + side / 2
+        number_of_steps = int(np.ceil(side / self.distance))
+        x_upper_limit = x_lower_limit + number_of_steps * self.distance
+        x_coordinates = np.linspace(x_lower_limit, x_upper_limit, number_of_steps)
+
+        lines = svg.Path()
+        for x in x_coordinates:
+            lines.append(svg.Line(x + y_upper_limit * 1j, x + y_lower_limit * 1j))
+        lines = lines.rotated(degs=self.rotation, origin=center)
+
+        return lines
+
+    def get_intersections_points(self):
+        """ Returns the intersection points between the path and the pattern lines as a dictionary
+        where the keys are the pattern lines and the values are the intersection points
+        """
+        intersections = sorted(self.pattern.intersect(self.path), key=lambda x: x[0][0])
+        # these intersections are sorted by the T value, which is the parameter of the pattern lines
+        # for two lines p & q: if p is closer to the start of the line than q, then Tp<Tq
+        # T value might be different within the same line, but it makes no difference for us
+
+        intersections_points = dict()
+        for (T1, seg1, t1), (T2, seg2, t2) in intersections:
+            if t2 * (t2 - 1) == 0:
+                # this means that the intersection is at the start or the end of the line
+                continue
+
+            # get the intersection point
+            intersections_points[seg1] = intersections_points.get(seg1, []) + [seg1.point(t1)]
+
+        return intersections_points
+
+    def get_filling_lines(self):
+        """ Returns the filling lines as a list of svgpathtools.path.Line objects
+        """
+        filling_lines = []
+        direction_flag = -1
+        for line, points in self.intersections_points.items():
+            direction_flag *= -1
+            for i in range(0, len(points)-1, 2): # TODO: check if this is correct
+                if direction_flag == 1:
+                    filling_lines.append(svg.Line(points[i], points[i + 1]))
+                else:
+                    filling_lines.append(svg.Line(points[i + 1], points[i]))
+
+        return filling_lines
 
 
 
-distance = 2
-rotation = 30
-# create number of points of distance between -90 and 270
-no_of_steps = int(np.ceil(360/distance))
-lower_limit = -90
-upper_limit = lower_limit + no_of_steps * distance
-# TODO: -90 = 0 - 90 & 270 = 180 + 90 which is the printer bed size
-x_points = np.linspace(lower_limit, upper_limit, no_of_steps)
+if __name__ == '__main__':
+    circle_path = svg.Path(svg.Arc(start=45, radius=45 + 45j, rotation=0, large_arc=1, sweep=1, end=135),
+                           svg.Arc(start=135, radius=45 + 45j, rotation=0, large_arc=1, sweep=1, end=45))
+    paths, attributes, svg_attributes = svg.svg2paths2('bitmap_Layer 1_1.svg')
+    wanted_path = paths[1]
 
-lines = svg.Path()
-for x in x_points:
-    lines.append(svg.Line(x + 270j, x - 90j))
-lines = lines.rotated(degs=rotation, origin=90+90j) # TODO: 90+90j is the center of the printer bed
+    # svg.disvg(wanted_path)
 
-# combined_path = svg.Path(*circle_path, *lines)
-# svg.disvg(combined_path)
-# print(lines.intersect(circle_path))
+    for path in [wanted_path, circle_path, paths[3]]:
+        fill = ShapeFillLine(path, rotation=30)
 
-intersections = sorted(lines.intersect(circle_path), key=lambda x: x[0][0])
-# print(intersections)
+        points = []
+        for v in fill.intersections_points.values():
+            points.extend(v)
 
-points_to_print = []
-lines_to_print = []
+        list_of_shapes = [*path,
+                         *fill.pattern,
+                         *fill.filling_lines]
 
-for (T1, seg1, t1), (T2, seg2, t2) in intersections:
-    if t2*(t2 - 1) == 0:
-        continue
+        attributes = len(path) * [{'stroke-width': 0.1, 'stroke': '#000000', 'fill':'None'}] + \
+                     len(fill.pattern) * [{'stroke-width': 0.1, 'stroke': '#00FF00', 'fill':'None'}] + \
+                     len(fill.filling_lines) * [{'stroke-width': 0.1, 'stroke': '#FF00FF'}]
 
-    points_to_print.append(seg2.point(t2))
+        svg.disvg(list_of_shapes, nodes=points, node_colors=['red'] * len(points),
+                  node_radii=[1] * len(points),
+                  attributes=attributes)
+        time.sleep(1)
 
-# read every two points and create a line
-for i in range(0, len(points_to_print), 2):
-    if i%2 == 0:
-        lines_to_print.append(svg.Line(points_to_print[i], points_to_print[i+1]))
-    else:
-        lines_to_print.append(svg.Line(points_to_print[i+1], points_to_print[i]))
-
-lines_to_print = svg.Path(*lines_to_print, *circle_path)
-svg.disvg(lines_to_print)
